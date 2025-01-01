be02f798c3dcd42a8f02d85174918a3b35dd1c23
DOTMAN-N <ogundimuadedotun49@gmail.com>
2025-01-01 23:17:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2348105464618"
+global.pairingNumber = "2348164199032"
 
-global.botNumber = ["2348105464618"]
+global.botNumber = ["2348164199032"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -18,7 +18,7 @@ global.antidelete = false
 global.autotyping = false
 global.autoBio = true
 global.antiSpam = true
-global.sign = "🌹"
+global.sign = "😎"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 global.bankname = "BLUE"
